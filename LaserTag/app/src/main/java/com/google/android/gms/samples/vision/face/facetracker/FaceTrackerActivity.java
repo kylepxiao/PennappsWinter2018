e1f0db3bf92993e22689b84acf54323d7a0cd886
y4smeen <y4smeen@gmail.com>
2018-01-20 15:43:07 -0500

    Animate laser

--- LaserTag/app/src/main/java/com/google/android/gms/samples/vision/face/facetracker/FaceTrackerActivity.java
+++ LaserTag/app/src/main/java/com/google/android/gms/samples/vision/face/facetracker/FaceTrackerActivity.java
@@ -129,6 +129,11 @@ public final class FaceTrackerActivity extends AppCompatActivity {
         //getCamera();
          mp = MediaPlayer.create(this, R.raw.lazer);
         Button fire = (Button) findViewById(R.id.fire);
+
+        ImageView demoImage = (ImageView)findViewById(R.id.empty);
+
+        int imagesToShow[] = { R.drawable.laser_01, R.drawable.laser_02, R.drawable.laser_03, R.drawable.laser_04, R.drawable.laser_05, R.drawable.laser_06 };
+
         fire.setOnClickListener(new View.OnClickListener()
         {
             @Override
@@ -136,6 +141,9 @@ public final class FaceTrackerActivity extends AppCompatActivity {
             {
                 Log.i("asdf", "FIRIN MAH LAZAR");
                 clicked = true;
+
+                animate(demoImage, imagesToShow, 0, false); 
+
                 new CountDownTimer(1000, 1000) {
                     public void onTick(long millisUntilFinished) {
                     }
@@ -144,6 +152,55 @@ public final class FaceTrackerActivity extends AppCompatActivity {
                         clicked = false;
                     }
                 }.start();
+
+                private void animate(final ImageView imageView, final int images[], final int imageIndex, final boolean forever) {
+
+                  //imageView <-- The View which displays the images
+                  //images[] <-- Holds R references to the images to display
+                  //imageIndex <-- index of the first image to show in images[] 
+                  //forever <-- If equals true then after the last image it starts all over again with the first image resulting in an infinite loop. You have been warned.
+
+                    int fadeInDuration = 500; // Configure time values here
+                    int timeBetween = 3000;
+                    int fadeOutDuration = 1000;
+
+                    imageView.setVisibility(View.INVISIBLE);    //Visible or invisible by default - this will apply when the animation ends
+                    imageView.setImageResource(images[imageIndex]);
+
+                    Animation fadeIn = new AlphaAnimation(0, 1);
+                    fadeIn.setInterpolator(new DecelerateInterpolator()); // add this
+                    fadeIn.setDuration(fadeInDuration);
+
+                    Animation fadeOut = new AlphaAnimation(1, 0);
+                    fadeOut.setInterpolator(new AccelerateInterpolator()); // and this
+                    fadeOut.setStartOffset(fadeInDuration + timeBetween);
+                    fadeOut.setDuration(fadeOutDuration);
+
+                    AnimationSet animation = new AnimationSet(false); // change to false
+                    animation.addAnimation(fadeIn);
+                    animation.addAnimation(fadeOut);
+                    animation.setRepeatCount(1);
+                    imageView.setAnimation(animation);
+
+                    animation.setAnimationListener(new AnimationListener() {
+                        public void onAnimationEnd(Animation animation) {
+                            if (images.length - 1 > imageIndex) {
+                                animate(imageView, images, imageIndex + 1,forever); //Calls itself until it gets to the end of the array
+                            }
+                            else {
+                                if (forever){
+                                animate(imageView, images, 0,forever);  //Calls itself to start the animation all over again in a loop if forever = true
+                                }
+                            }
+                        }
+                        public void onAnimationRepeat(Animation animation) {
+                            // TODO Auto-generated method stub
+                        }
+                        public void onAnimationStart(Animation animation) {
+                            // TODO Auto-generated method stub
+                        }
+                    });
+                }
                 //mp.start();
                 //turnOnFlash();
                 /*mCameraSource.takePicture(null, new PictureCallback(){
